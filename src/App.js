6abcceecaa5a71931ca51683b4f8a8dcba744543
Aravind-Kumar-2000 <aravindmpkas@gmail.com>
2023-12-27 13:04:13 +0530

    React Custom Hook

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import UseRefHook from "./Snippets/useRef";
 import UseEffectHook from "./Snippets/useEffect";
 import API from "./Snippets/API_Fetch/API";
 import EntryPoint from "./Snippets/CRUD_Operation/entry";
+import App02 from "./Snippets/Custom_Hook/App02";
 import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
@@ -30,6 +31,9 @@ function App() {
           <li>
             <Link to={"/crud"}>API Fetch</Link>
           </li>
+          <li>
+            <Link to={"/customhook"}>React Custom Hook</Link>
+          </li>
         </ul>
         <div>
           <Link to={"/"}>Back to Home</Link>
@@ -42,6 +46,7 @@ function App() {
         <Route path="/useEffect" element={<UseEffectHook />} />
         <Route path="/apifetch" element={<API />} />
         <Route path="/crud" element={<EntryPoint />} />
+        <Route path="/customhook" element={<App02 />} />
       </Routes>
     </div>
   );
